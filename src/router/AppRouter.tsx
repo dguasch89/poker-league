1448b4ac96f7108fdfa72151280ed299c7845274
DanielCabiscol <danielcabiscol@gmail.com>
2023-02-01 09:55:50 +0100

    calendar page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,4 +1,5 @@
 import {Route, Routes} from 'react-router-dom';
+import {Calendar} from '../views/Calendar';
 import {HomePage} from '../views/HomePage/HomePage';
 import {Players} from '../views/Players';
 import {Rules} from '../views/Rules';
@@ -21,6 +22,7 @@ export const AppRouter = () => {
       <Route path={mainRoute} element={<HomePage />} />
       <Route path={routes.players} element={<Players />} />
       <Route path={routes.rules} element={<Rules />} />
+      <Route path={routes.calendar} element={<Calendar />} />
       {/* <Route path="*" element={<PageNotFound />} /> */}
       {/* <Route path={ERROR} element={<ErrorPage />} /> */}
     </Routes>
---
+++ src/views/Calendar.tsx
@@ -0,0 +1,9 @@
+export function Calendar() {
+  return (
+    <div className="p-20 bg-indigo-900">
+      <h2 className="font-bold text-center text-2xl text-white italic ">
+        The schedule of season 2 is not defined yet
+      </h2>
+    </div>
+  );
+}
