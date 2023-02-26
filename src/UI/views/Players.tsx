f296f125318753ac2f6d83a41a726f15e0dab13c
Joan Bruguera <joanbrugueram@gmail.com>
2023-02-26 14:08:52 +0000

    fix: order /seasons page by last season points
    the page didn't really have a defined order before (see previous commits),
    so sort by last season position to give it a well-defined order

--- src/UI/views/Players.tsx
+++ src/UI/views/Players.tsx
@@ -1,12 +1,14 @@
 import {Player} from '../components/Player';
 import {IPlayer} from '../../domain/interfaces';
 import {usePlayersStore} from '../../state/players';
+import {sortBy} from '../../domain/util.js';
 
 export function Players() {
   const players: IPlayer[] = usePlayersStore.getState().players || [];
+  const sortedPlayers = sortBy(players, (p: IPlayer) => p.lastSeasonPosition, 'asc');
   return (
     <div className="Players p-4 sm:grid sm:grid-cols-4 gap-4 flex flex-col">
-      {[...players].reverse().map((player: IPlayer) => (
+      {sortedPlayers.map((player: IPlayer) => (
         <Player key={player.id} player={player} />
       ))}
     </div>
