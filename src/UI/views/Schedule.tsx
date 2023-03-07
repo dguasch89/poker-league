33ced1f1aaa1fe9e1db93716a5c9f9a86f109c34
Albert Mosella <mosella.albert@gmail.com>
2023-03-07 16:00:06 +0100

    fix: get winner from standings

--- src/UI/views/Schedule.tsx
+++ src/UI/views/Schedule.tsx
@@ -1,40 +1,57 @@
-import {format, formatDistanceToNowStrict} from 'date-fns';
-import isPast from 'date-fns/isPast';
-import {IGameSchedule, IPlayer} from '../../domain/interfaces';
-import {getPlayerNickName} from '../../domain/player';
-import {useGamesScheduleStore} from '../../state/games-schedule';
-import {usePlayersStore} from '../../state/players';
-import goldMedal from '../assets/gold_medal.png';
-import {appendOrdinal} from '../utils/ordinals';
+import { format, formatDistanceToNowStrict } from "date-fns";
+import isPast from "date-fns/isPast";
+import { IGameSchedule, IPlayer, ISeason } from "../../domain/interfaces";
+import { getGameWinner, getPlayerNickName } from "../../domain/player";
+import { useGamesScheduleStore } from "../../state/games-schedule";
+import { usePlayersStore } from "../../state/players";
+import { useSeasonsStore } from "../../state/seasons";
+import goldMedal from "../assets/gold_medal.png";
+import { appendOrdinal } from "../utils/ordinals";
 
 export function Schedule() {
-  const gamesSchedule: IGameSchedule[] = useGamesScheduleStore.getState().gamesSchedule;
+  const gamesSchedule: IGameSchedule[] =
+    useGamesScheduleStore.getState().gamesSchedule;
   const players: IPlayer[] = usePlayersStore.getState().players;
+  const seasons: ISeason[] = useSeasonsStore.getState().seasons;
 
   return (
     <>
       <div className="banner flex flex-col gap-2 items-center justify-items-center bg-indigo-900 p-5">
-        <h2 className="font-bold text-center text-2xl text-white italic ">Season 2 schedule</h2>
+        <h2 className="font-bold text-center text-2xl text-white italic ">
+          Season 2 schedule
+        </h2>
       </div>
       <div className="flex flex-col justify-center items-center w-full py-4 px-1">
-        {gamesSchedule.map(g => (
+        {gamesSchedule.map((g) => (
           <div
             key={g.id}
             className="grid gap-0 sm:gap-4 grid-cols-schedule justify-center items-center rounded-lg m-2 transform 
-                                transition duration-500 hover:scale-110 cursor-pointer border-4 border-orange-500 text-xs sm:text-xl lg:text-3xl lg:p-2 font-semibold text-white bg-slate-800"
+                                transition duration-500 hover:scale-110 cursor-pointer border-4 border-orange-500 text-xs sm:text-xl lg:text-2xl lg:p-2 font-semibold text-white bg-slate-800"
           >
             <span className="text-center border-r">{appendOrdinal(g.id)}</span>
-            <span className="p-1 text-center">{format(new Date(g.date), "do 'of' MMMM")}</span>
-            <span className="pr-2 text-center border-r">{format(new Date(g.date), "HH:mm")}</span>
+            <span className="p-1 text-center">
+              {format(new Date(g.date), "do 'of' MMMM")}
+            </span>
+            <span className="pr-2 text-center border-r">
+              {format(new Date(g.date), "HH:mm")}
+            </span>
             <div className="flex p-1 sm:pl-4 justify-end items-center">
               {isPast(new Date(g.date)) ? (
                 <div className="flex justify-center items-center">
-                  {g.winner
-                  ? <>
-                    <img src={goldMedal} className="w-4 sm:w-8 lg:w-12" alt="gold-medal" />
-                    <span className="pl-1">{getPlayerNickName(g.winner, players)}</span>
-                  </>
-                  : <span className="pl-1">⏱ Coming soon!</span>}
+                  {getGameWinner(g.id, 2, players, seasons) ? (
+                    <>
+                      <img
+                        src={goldMedal}
+                        className="w-4 sm:w-8"
+                        alt="gold-medal"
+                      />
+                      <span className="pl-1">
+                        {getGameWinner(g.id, 2, players, seasons)}
+                      </span>
+                    </>
+                  ) : (
+                    <span className="pl-1">⏱ Coming soon!</span>
+                  )}
                 </div>
               ) : (
                 <div>
--- src/domain/player.ts
+++ src/domain/player.ts
@@ -1,12 +1,29 @@
-import {IPlayer} from './interfaces';
+import { IPlayer, ISeason } from "./interfaces";
 
 export const isInvalidPlayer = (playerId: number) => {
-  return playerId === null || playerId === undefined || playerId < 0 || typeof playerId != 'number';
+  return (
+    playerId === null ||
+    playerId === undefined ||
+    playerId < 0 ||
+    typeof playerId != "number"
+  );
 };
 
-export const getPlayerNickName = (playerId: number, players: IPlayer[]) => {
+export const getPlayerNickName = (playerId: any, players: IPlayer[]) => {
   if (playerId) {
-    const winner = players.find(p => p.id === playerId);
-    return winner?.nickname;
+    const playerNickName = players.find((p) => p.id === playerId);
+    return playerNickName?.nickname;
   }
+};
+
+export const getGameWinner = (
+  id: number,
+  seasonId: number,
+  players: IPlayer[],
+  seasons: ISeason[]
+) => {
+  const winnerId = seasons[seasonId - 1]?.games?.find((game) => game.id === id)
+    ?.standings[0];
+
+  return getPlayerNickName(winnerId, players);
 };
