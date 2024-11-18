d691d2fc292b53a98c99e2be3cfa766f1e2e52e4
DanielCabiscol <danielcabiscol@gmail.com>
2024-11-18 20:12:56 +0100

    fix: style detail season columns

--- src/UI/components/SeasonDetail.tsx
+++ src/UI/components/SeasonDetail.tsx
@@ -7,13 +7,32 @@ interface SeasonDetailProps {
   season: ISeason;
 }
 
+const getColsBySeasonType = (seasonType: number) => {
+  switch (seasonType) {
+    case 1:
+    case 2:
+      return 10;
+    case 3:
+      return 15;
+    case 4:
+      return 12;
+    default:
+      return 10;
+  }
+};
+
 export function SeasonDetail(props: SeasonDetailProps) {
   const players = usePlayersStore.getState().players.filter((p: IPlayer) => p.active);
 
   return (
     <div>
       <div className="border rounded-md shadow-lg max-sm:hidden">
-        <div className="grid grid-cols-seasonDetail justify-between pl-4 pr-4 gap-4 bg-slate-800 rounded-t-md">
+        <div
+          style={{
+            gridTemplateColumns: `12% repeat(${getColsBySeasonType(props.season.type)}, 1fr)`,
+          }}
+          className="grid justify-between pl-4 pr-4 gap-4 bg-slate-800 rounded-t-md"
+        >
           <div className="font-bold uppercase text-amber-500 text-xs sm:text-sm pt-4 pb-4 truncate">
             Game/Player
           </div>
@@ -29,7 +48,10 @@ export function SeasonDetail(props: SeasonDetailProps) {
         {players.map((p: IPlayer) => (
           <div
             key={p.id}
-            className="grid grid-cols-seasonDetail w-full p-2 gap-4 border-b border-l-4 border-l-amber-500 items-center"
+            style={{
+              gridTemplateColumns: `12% repeat(${getColsBySeasonType(props.season.type)}, 1fr)`,
+            }}
+            className="grid w-full p-2 gap-4 border-b border-l-4 border-l-amber-500 items-center"
           >
             <div className="text-xs font-bold text-indigo-800">{p.nickname}</div>
             {props.season.games.map((g: IGame) => (
